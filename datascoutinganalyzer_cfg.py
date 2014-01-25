79f16f86f3656a6a6520df62462d87f140e62ada
isildakbora <bora.isildak@cern.ch>
2014-01-25 09:24:46 +0200

    Update datascoutinganalyzer_cfg.py

--- datascoutinganalyzer_cfg.py
+++ datascoutinganalyzer_cfg.py
@@ -1,6 +1,17 @@
 import FWCore.ParameterSet.Config as cms
-
 process = cms.Process("Demo")
+
+process.load("CommonTools.RecoAlgos.HBHENoiseFilterResultProducer_cfi")
+
+process.load("RecoMET.METFilters.hcalLaserEventFilter_cfi")
+process.hcalLaserEventFilter.taggingMode = True
+process.hcalLaserEventFilter.vetoByRunEventNumber = False
+process.hcalLaserEventFilter.vetoByHBHEOccupancy= True
+
+process.load("RecoMET.METFilters.eeBadScFilter_cfi")
+process.eeBadScFilter.taggingMode = True
+
+
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
@@ -11,12 +22,13 @@ process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.GlobalTag.globaltag = 'GR_P_V41_AN1::All'
 
 process.load('JetMETCorrections.Configuration.DefaultJEC_cff')
-process.MessageLogger.cerr.FwkReport.reportEvery = 10
+
+process.MessageLogger.cerr.FwkReport.reportEvery = 10000
 process.options = cms.untracked.PSet(
     SkipEvent = cms.untracked.vstring('ProductNotFound'),
     )
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
@@ -72,22 +84,40 @@ process.source = cms.Source("PoolSource",
 )
 )
 
-plugin_type       = [["CaloScoutingAnalyzer","ak5CaloJets","ak5CaloL1L2L3","kt6CaloJets"],["PFJetScoutingAnalyzer","ak5PFJets","ak5PFL1L2L3","kt6PFJets"]]
-i = 1 # 0 is CaloScoutingAnalyzer 1 is PFJetScoutingAnalyzer
+VERTEX_SEL=("!isFake && ndof > 4 && abs(z) <= 24 && position.Rho <= 2")
+
+process.goodPrimaryVertices = cms.EDFilter("VertexSelector",
+                                   src = cms.InputTag("offlinePrimaryVertices"),
+                                   cut = cms.string(VERTEX_SEL),
+                                   filter = cms.bool(True),
+                                   )
+
+plugin_type       = [["CaloScoutingAnalyzer","ak5CaloJets","ak5CaloL1L2L3Residual","kt6CaloJets"],["PFJetScoutingAnalyzer","ak5PFJets","ak5PFL1FastL2L3Residual","kt6PFJets"]]
+i = 0 # 0 is CaloScoutingAnalyzer 1 is PFJetScoutingAnalyzer
 
 print plugin_type[i][0]+" is running."+"\njets:"+plugin_type[i][1]+"\njetCorrections:"+plugin_type[i][2]+"\nrho:"+plugin_type[i][3]
-
+if i==0:
+	output = 'test_Calo.root'
+else:
+	output = 'test_PF.root'
 process.demo = cms.EDAnalyzer(plugin_type[i][0],
                               jets = cms.InputTag(plugin_type[i][1]),
-                              jetCorrections = cms.string(plugin_type[i][2]),
+                              HBHENoiseFilterResultLabel = cms.InputTag("HBHENoiseFilterResultProducer", "HBHENoiseFilterResult"),
+                              apply_corrections_reco = cms.bool(True),
+                              apply_corrections_DS = cms.bool(True),
+                              jetCorrectionsReco = cms.string(plugin_type[i][2]),
+                              jetCorrectionsDS = cms.string("ak5CaloL2L3Residual"),
                               rho  = cms.InputTag(plugin_type[i][3],"rho"),
                               jetThreshold = cms.double(20),
                               met = cms.InputTag("met"),
                               electrons = cms.InputTag(""),
                               muons = cms.InputTag(""),
                               noise = cms.InputTag(""),
-                              outputFile = cms.string("test.root")
+                              outputFile = cms.string(output)
 
 )
-process.p = cms.Path(process.demo)
+
+process.metOptionalFilterSequence = cms.Sequence(process.HBHENoiseFilterResultProducer * process.hcalLaserEventFilter * process.eeBadScFilter)
+#process.goodPrimaryVertices *
+process.p       = cms.Path(  process.goodPrimaryVertices * process.metOptionalFilterSequence * process.demo)
 
